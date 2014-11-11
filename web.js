95e836054294dd7bbb61c616f6558cbf118b3324
Andrew Kos <akos@theonion.com>
2014-11-11 16:57:48 -0600

    Generate random articles

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var open = require('open');
+var loreumIpsum = require('lorem-ipsum');
 
 var app = connect();
 
@@ -8,9 +9,31 @@ var app = connect();
 var PORT = 3000;
 var HOME_PAGE = 'example.html';
 
-// start server
+// set up 3rd party middleware
 app.use(serveStatic('example', {index: [HOME_PAGE]}));
 app.use(serveStatic('bower_components', {index: false, extensions: ['html', 'css']}));
+
+// make endpoints for some test articles
+app.use('/article', function articleMiddleware(req, res, next) {
+
+  var article = {
+    title: loreumIpsum({
+      sentenceLowerBound: 2,
+      sentenceUpperBound: 8
+    }).replace('.', ''),
+    body: loreumIpsum({
+      units: 'paragraphs',
+      paragraphLowerBound: 1,
+      paragraphUpperBound: 10
+    }).replace('\r\n', '')
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(article));
+});
+
+
+// start server
 app.listen(PORT);
 
 // open browser
