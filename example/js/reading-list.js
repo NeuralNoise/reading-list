4ed5f2765353fdc066bad4e618c47f366f209b7e
Andrew Kos <akos@theonion.com>
2014-11-12 11:39:59 -0600

    Some eventing in place

--- example/js/reading-list.js
+++ example/js/reading-list.js
@@ -8,16 +8,29 @@
     return window.innerHeight || window.document.documentElement.clientHeight;
   };
 
-  var inView = function (el, offset) {
-    return el.getBoundingClientRect().top <= (windowHeight() - offset) &&
-            el.getBoundingClientRect().bottom >= offset;
+  /**
+   * Check that some part of the article is in view + threshold.
+   */
+  var withinLoadingThreshold = function (el, threshold) {
+    // window dimesions
+    var top = -threshold;
+    var bot = windowHeight() + threshold;
+
+    // check if element bounding box is within window dimensions
+    var elBounding = el.getBoundingClientRect();
+    var overTop = elBounding.top < top && elBounding.bottom < top;
+    var belowBot = elBounding.top > bot && elBounding.bottom > bot;
+
+    // if the whole thing is not over the top, or the whole thing is not below the bot,
+    //  some part of it must be in the view
+    return !(overTop || belowBot);
   };
 
   $document.ready(function () {
 
     var $readingListContent = $('#readingListContent');
     var $readingListArticles = $('#readingListArticles');
-    var $sections = $readingListArticles.find('.article,.adspace');
+    var $readingListItems = $readingListArticles.find('.reading-list-item');
 
     if ($.browser.mobile) {
       // mobile browser, use IScroll
@@ -26,41 +39,64 @@
       }));
     }
 
+    /**
+     * Reading list event framework.
+     */
     var eventing = function () {
       var scrollTop = $readingListContent.scrollTop();
       if (scrollTop <= 0) {
+        // we're at the top of the reading list
         $readingListContent.trigger('reading-list-at-top');
       } else if (scrollTop + windowHeight() >= $readingListArticles.height()) {
+        // we're at the bottom of the reading list
         $readingListContent.trigger('reading-list-at-bottom');
       }
 
-      // TODO : add list of things, along with offsets to check if they are in view, maybe have data attrs
+      // see if anything is in view and determine if something should start loading
+      $readingListItems.each(function (i, item) {
+        var $item = $(item);
+        var inThreshold = withinLoadingThreshold(item, 0);
+        if(inThreshold && !$item.hasClass('in-threshold')) {
+          $item.addClass('in-threshold');
+          $readingListContent.trigger('reading-list-item-in-view', $item);
+        } else if (!inThreshold && $item.hasClass('in-threshold')){
+          $item.removeClass('in-threshold');
+          $readingListContent.trigger('reading-list-item-out-view', $item);
+        }
+      });
     };
 
+    $readingListContent.on('reading-list-item-in-view', function (event, item) {
+      var $item = $(item);
+      var name;
+      if ($item.children('article').length > 0) {
+        name = $item.children('article').attr('id');
+      } else {
+        name = 'adspace';
+      }
+      console.log('in view', name);
+    });
+    $readingListContent.on('reading-list-item-out-view', function (event, item) {
+      var $item = $(item);
+      var name;
+      if ($item.children('article').length > 0) {
+        name = $item.children('article').attr('id');
+      } else {
+        name = 'adspace';
+      }
+      console.log('out view', name);
+    });
+    $readingListContent.on('reading-list-at-top', function () {
+      console.log('topped', $readingListContent.scrollTop());
+    });
+    $readingListContent.on('reading-list-at-bottom', function () {
+      console.log('bottomed', $readingListContent.scrollTop());
+    });
+
+    eventing();
     $readingListContent.on('scroll', function () {
+// TODO : might want to do some kind of debounce here
       eventing();
-
-      $sections.each(function (i, article) {
-
-        if(inView(article, -300)) {
-          article.style.backgroundColor =  '#00f';
-        } else {
-          article.style.backgroundColor = '';
-        }
-
-      });
-
-      // START DEBUG STUFF
-      $('#debugScrollHeight').html('scroll: ' + $readingListContent.scrollTop());
-      $('#debugInnerHeight').html('innerHeight: ' + (window.innerHeight || window.document.documentElement.clientHeight));
-      // END DEBUG STUFF
-    });
-
-    $readingListContent.on('reading-list-at-top', function (events) {
-      console.log('topped ' + $readingListContent.scrollTop());
-    });
-    $readingListContent.on('reading-list-at-bottom', function (events) {
-      console.log('bottomed ' + $readingListContent.scrollTop());
     });
 
   });
